8ca76cc9f4dc7f411740fb8eeaa227855b7c6d67
Alexander-Jordan <curtjordan93@gmail.com>
2025-06-06 11:43:06 +0200

    Add GameManager and modes, and add a start function for pacman to do stuff when mode changes to PLAYING

--- entities/pacman/pacman.gd
+++ entities/pacman/pacman.gd
@@ -2,17 +2,36 @@ class_name Pacman extends GridTraveller
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
+var input_direction_map: Dictionary[String, Vector2i] = {
+	'up': Vector2i.UP,
+	'left': Vector2i.LEFT,
+	'down': Vector2i.DOWN,
+	'right': Vector2i.RIGHT,
+}
+
 func _process(delta: float) -> void:
 	super(delta)
 	if coords_move_to is Vector2i:
 		global_position = global_position.move_toward(coords_move_to, delta*speed)
 
+func _ready() -> void:
+	GM.mode_changed.connect(on_game_mode_changed)
+
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed('up'):
-		tile_direction = Vector2i.UP
-	if event.is_action_pressed('down'):
-		tile_direction = Vector2i.DOWN
-	if event.is_action_pressed('left'):
-		tile_direction = Vector2i.LEFT
-	if event.is_action_pressed('right'):
+	if GM.mode != GM.Mode.PLAYING:
+		return
+	
+	for direction in input_direction_map:
+		if event.is_action_pressed(direction):
+			tile_direction = input_direction_map[direction]
+	animated_sprite_2d.look_at(position + Vector2(tile_direction))
+
+func on_game_mode_changed(mode: GM.Mode) -> void:
+	match mode:
+		GM.Mode.PLAYING:
+			start()
+
+func start() -> void:
+	if tile_direction == Vector2i.ZERO:
 		tile_direction = Vector2i.RIGHT
+	animated_sprite_2d.play('default')
---
+++ utilities/game_manager/game_manager.gd
@@ -0,0 +1,24 @@
+class_name GameManager extends Node
+
+enum Mode {
+	NONE,
+	PLAYING,
+	OVER,
+}
+
+var mode: Mode = Mode.NONE:
+	set(m):
+		if !Mode.values().has(m) or m == mode:
+			return
+		mode = m
+		mode_changed.emit(m)
+
+signal mode_changed(mode: Mode)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed('dev_mode_none'):
+		mode = Mode.NONE
+	if event.is_action_pressed('dev_mode_playing'):
+		mode = Mode.PLAYING
+	if event.is_action_pressed('dev_mode_over'):
+		mode = Mode.OVER
